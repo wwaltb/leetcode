a2f7d23810518c3c80087a191ce403dbc7899e0d
wwaltb <wwaltb@proton.me>
2025-02-25 17:59:39 -0800

    Solved 49 - Group Anagrams

--- 49.group-anagrams.c
+++ 49.group-anagrams.c
@@ -4,16 +4,45 @@
 #include <string.h>
 
 // @leet start
+struct HashEntry {
+    int id;
+    char* str;
+    UT_hash_handle hh;
+};
+
+struct HashEntry* hashFind(struct HashEntry* head, int id)
+{
+    struct HashEntry* ent;
+    HASH_FIND_INT(head, &id, ent);
+    return ent;
+}
+
+void hashAdd(struct HashEntry** head, int id, char* str)
+{
+    struct HashEntry* ent = malloc(sizeof(struct HashEntry));
+    ent->id = id;
+    ent->str = str;
+    HASH_ADD_INT(*head, id, ent);
+}
+
+void hashFree(struct HashEntry** head)
+{
+    struct HashEntry *ent, *temp;
+    HASH_ITER(hh, *head, ent, temp)
+    {
+        HASH_DEL(*head, ent);
+        free(ent);
+    }
+}
+
 int cmp_chars(const void* a, const void* b)
 {
     return *(char*)a - *(char*)b;
 }
 
-int cmp_strlens(const void* a, const void* b)
+int cmp_by_str(const struct HashEntry* a, const struct HashEntry* b)
 {
-    const char* str1 = *(const char**)a;
-    const char* str2 = *(const char**)b;
-    return (int)strlen(str1) - (int)strlen(str2);
+    return strcmp(a->str, b->str);
 }
 
 /**
@@ -23,16 +52,82 @@ int cmp_strlens(const void* a, const void* b)
  */
 char*** groupAnagrams(char** strs, int strsSize, int* returnSize, int** returnColumnSizes)
 {
-    qsort(strs, strsSize, sizeof(char*), cmp_strlens);
-    int prev_len = 0;
+    *returnSize = 0;
+    int capacity = 10;
+    char*** result = malloc(capacity * sizeof(char**));
+    *returnColumnSizes = malloc(capacity * sizeof(int));
+
+    // copy sorted strs into a hash with their index as the id
+    struct HashEntry* strs_hash = NULL;
+
     for (int i = 0; i < strsSize; i++) {
+        // create a copy of the str
         int len = (int)strlen(strs[i]);
-        if (len > prev_len) {
-        }
-        qsort(strs[i], len, sizeof(char), cmp_chars);
-        printf("%s\n", strs[i]);
+        char* str_cpy = malloc((len + 1) * sizeof(char));
+        strcpy(str_cpy, strs[i]);
+        // sort its characters and add it to the hash
+        qsort(str_cpy, len, sizeof(char), cmp_chars);
+        hashAdd(&strs_hash, i, str_cpy);
     }
 
-    return NULL;
+    // sort the hash by the strings alphabetically
+    HASH_SORT(strs_hash, cmp_by_str);
+
+    // create a result group
+    int col_size = 0;
+    int col_capacity = 4;
+    char** result_col = malloc(col_capacity * sizeof(char*));
+
+    char* prev_str = NULL;
+    struct HashEntry* ent = strs_hash;
+    // iterate through the hash strings and group duplicates, putting the real strings into result_col
+    while (ent != NULL) {
+        // first string in new group or grouped anagram, so add to result column
+        if (prev_str == NULL || strcmp(prev_str, ent->str) == 0) {
+            if (col_size == col_capacity) {
+                col_capacity *= 2;
+                result_col = realloc(result_col, col_capacity * sizeof(char*));
+            }
+
+            result_col[col_size] = strs[ent->id];
+            col_size += 1;
+
+            prev_str = ent->str;
+            ent = ent->hh.next;
+        }
+        // otherwise, this is a new anagram group so add the last col and start a new one
+        else {
+            // add to result
+            if (*returnSize == capacity) {
+                capacity *= 2;
+                result = realloc(result, capacity * sizeof(char**));
+                *returnColumnSizes = realloc(*returnColumnSizes, capacity * sizeof(int));
+            }
+            result[*returnSize] = result_col;
+            (*returnColumnSizes)[*returnSize] = col_size;
+            *returnSize += 1;
+
+            // reset result_col, add this string to it and clear prev_str
+            col_size = 0;
+            col_capacity = 4;
+            result_col = malloc(col_capacity * sizeof(char*));
+
+            prev_str = NULL;
+        }
+    }
+
+    // add the last column to the result
+    if (*returnSize == capacity) {
+        capacity *= 2;
+        result = realloc(result, capacity * sizeof(char**));
+        *returnColumnSizes = realloc(*returnColumnSizes, capacity * sizeof(int));
+    }
+    result[*returnSize] = result_col;
+    (*returnColumnSizes)[*returnSize] = col_size;
+    *returnSize += 1;
+
+    hashFree(&strs_hash);
+
+    return result;
 }
 // @leet end
